eeaee6805f0bc2aeb18734e0a14e5e423ecedcc4
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-11-15 10:46:40 -0800

    Adds show and index view for gifts

--- skeleton/app/views/api/guests/show.json.jbuilder
+++ skeleton/app/views/api/guests/show.json.jbuilder
@@ -1,6 +1,6 @@
 # json.extract! @guest, :name, :age, :favorite_color
+json.partial! 'api/guests/guest', guest: @guest
+
 json.gifts do
   json.array! @guest.gifts, :title, :description
 end
-
-json.partial! 'api/guests/guest', guest: @guest
--- skeleton/config/routes.rb
+++ skeleton/config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
-    # Your routes here
-    resources :parties, only: [:index, :show]
-    resources :gifts, only: [:show]
     resources :guests, only: [:index, :show] do
       resources :gifts, only: [:index]
     end
+    resources :parties, only: [:index, :show]
+    resources :gifts, only: [:show]
+
   end
 end
